a25ac73a73091db22b6807fb8b92a611dfe68f80
Statikman619 <martinez.michael02@gmail.com>
2021-08-17 19:50:33 -0700

    working on my reducers

--- client/src/utils/reducers.js
+++ client/src/utils/reducers.js
@@ -1,4 +1,24 @@
-reducer = (state = defaultState, action) => {
+import {
+  UPDATE_PRODUCTS,
+  UPDATE_CATEGORIES,
+  UPDATE_CURRENT_CATEGORY,
+  ADD_TO_CART,
+  ADD_MULTIPLE_TO_CART,
+  REMOVE_FROM_CART,
+  UPDATE_CART_QUANTITY,
+  CLEAR_CART,
+  TOGGLE_CART,
+} from "./actions";
+
+const defaultState = {
+  products: [],
+  cart: [],
+  cartOpen: false,
+  categories: [],
+  currentCategory: "",
+};
+
+const reducer = (state = defaultState, action) => {
   switch (action.type) {
     //if action type value is the value of 'UPDATE_PRODUCTS', return a new state object with an updated products array
     case UPDATE_PRODUCTS:
